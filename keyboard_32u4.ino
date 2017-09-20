889e67703584effe628169d3ad165af24dbc61dc
Virtorgan <32040318+virtorgan@users.noreply.github.com>
2017-09-20 22:04:25 +0200

    Reading macro cleanup and comment addition

--- keyboard_32u4.ino
+++ keyboard_32u4.ino
@@ -1,12 +1,14 @@
-#define Reading7 ((PINB & (1<<6)) << 1)
-#define Reading6 ((PINB & (1<<2)) << 4)
-#define Reading5 ((PINB & (1<<3)) << 2)
-#define Reading4 ((PINB & (1<<1)) << 3)
-#define Reading57 ((PINF & b11110000) >> 4)
-#define Reading (Reading7 | Reading6 | Reading5 | Reading4 | Reading57)
-#ifndef cbi
+#define Reading7 ((PINB & (1<<6)) << 1) // pin 10 as bit 7 of Reading value
+#define Reading6 ((PINB & (1<<2)) << 4) // pin 16 as bit 6 of Reading value
+#define Reading5 ((PINB & (1<<3)) << 2) // pin 14 as bit 5 of Reading value
+#define Reading4 ((PINB & (1<<1)) << 3) // pin 15 as bit 4 of Reading value
+#define Reading30 ((PINF & b11110000) >> 4) // pin A0,A1,A2,A3 as bit 3,2,1,0 of Reading value
+#define Reading (Reading7 | Reading6 | Reading5 | Reading4 | Reading30) // concatenation to one byte
+
+/*#ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
 #endif
 #ifndef sbi
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
+*/
